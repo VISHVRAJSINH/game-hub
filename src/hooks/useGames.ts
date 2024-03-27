4951c4539f72a1f06546072e0ef05020d09d1f64
VISHVARAJSINH <you@example.com>
2024-03-27 17:53:02 +0530

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -18,7 +18,7 @@ export interface Game {
   
 const useGames = (gameQuery : GameQuery) => useData<Game>('/games',
 {
-  params : {genres : gameQuery.genre?.id, platform:gameQuery.platform?.id}
+  params : {genres : gameQuery.genre?.id, platform:gameQuery.platform?.id, ordering:gameQuery.sortOrder}
 },
 [gameQuery]);
 
