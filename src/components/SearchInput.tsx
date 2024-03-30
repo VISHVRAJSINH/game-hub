a46c22134067b9fda5ab6e71075d963c6a06b8dd
VISHVARAJSINH <you@example.com>
2024-03-30 14:56:04 +0530

    Build search input

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,14 @@
+import { Input } from "@chakra-ui/react";
+
+const SearchInput = () => {
+  return (
+    <Input
+      size="md"
+      borderRadius={6}
+      placeholder="Search games"
+      variant="filled"
+    />
+  );
+};
+
+export default SearchInput;
